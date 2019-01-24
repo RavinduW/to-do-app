a4b13130a47a28dcb3a322a1f48b095b5d7cb234
RavinduW <raviduchinthaka3@gmail.com>
2019-01-24 21:09:05 +0530

    MongoDB database configuration with mlab

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser'); //body-parser module
+var mongoose = require('mongoose'); //mongoose package
+
+//connect to the database(mongoDB)
+mongoose.connect('mongodb://test:123test@ds211635.mlab.com:11635/to_do',{useNewUrlParser: true }); //mlab
+
+//create a schema(blueprint)
+var todoSchema = new mongoose.Schema({
+    item:String
+});
+
+var TodoModel = mongoose.model('Todo',todoSchema); //model
+var ItemOne = TodoModel({item:'buy books'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item:'get milk'},{item:'take a shower'},{item:'do the coding tutorials'}]; 
 var urlencodedParser = bodyParser.urlencoded({extended:false}); //middleware to post request

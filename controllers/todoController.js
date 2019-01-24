e91711476ffd6930fad253da693814f71024b6b1
RavinduW <raviduchinthaka3@gmail.com>
2019-01-24 22:05:48 +0530

    Add and delete to-do items from database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,30 +10,42 @@ var todoSchema = new mongoose.Schema({
 });
 
 var TodoModel = mongoose.model('Todo',todoSchema); //model
-var ItemOne = TodoModel({item:'buy books'}).save(function(err){
-    if(err) throw err;
-    console.log('item saved');
-});
 
-var data = [{item:'get milk'},{item:'take a shower'},{item:'do the coding tutorials'}]; 
+//var ItemOne = TodoModel({item:'buy books'}).save(function(err){
+//    if(err) throw err;
+//    console.log('item saved');
+//});
+
+//var data = [{item:'get milk'},{item:'take a shower'},{item:'do the coding tutorials'}]; 
 var urlencodedParser = bodyParser.urlencoded({extended:false}); //middleware to post request
 
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('home',{todos:data}); //compact the data objects
+        TodoModel.find({},function(err,data){ //find data from Todo model of database
+            if(err) throw err;
+            res.render('home',{todos:data});
+        });
+        //res.render('home',{todos:data}); //compact the data objects
     });
 
     app.post('/todo',urlencodedParser,function(req,res){
-        data.push(req.body);
-        res.json(data); //response for the front end
+        var newTodo = TodoModel(req.body).save(function(err,data){
+            if(err) throw err;
+            res.json(data);
+        });
+        //data.push(req.body);
+        //res.json(data); //response for the front end
     });
 
     app.delete('/todo/:item',function(req,res){
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g,'-') !== req.params.item; //post request parameters fetch the spaces with hyphens
+        TodoModel.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+            if(err) throw err;
+            res.json(data);
         });
-        res.json(data);
+        //data = data.filter(function(todo){
+        //    return todo.item.replace(/ /g,'-') !== req.params.item; //post request parameters fetch the spaces with hyphens
+        //});
+        //res.json(data);
     });
-
 };

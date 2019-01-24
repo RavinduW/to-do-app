1d91dd21d449b19564152c117585fc722b341f4a
RavinduW <raviduchinthaka3@gmail.com>
2019-01-24 18:15:46 +0530

    Ajax requests for add and delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,24 @@
+var bodyParser = require('body-parser'); //body-parser module
+
+var data = [{item:'get milk'},{item:'take a shower'},{item:'do the coding tutorials'}]; 
+var urlencodedParser = bodyParser.urlencoded({extended:false}); //middleware to post request
+
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('home');
+        res.render('home',{todos:data}); //compact the data objects
     });
 
-    app.post('/todo',function(req,res){
-
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data); //response for the front end
     });
 
-    app.delete('/todo',function(req,res){
-
+    app.delete('/todo/:item',function(req,res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item; //post request parameters fetch the spaces with hyphens
+        });
+        res.json(data);
     });
 
 };
